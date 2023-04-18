1467dd8b8adbe324f18f74b276ed8f32ed8b6957
Brendan McShane <brendanmcs7@gmail.com>
2023-04-17 20:51:02 -0400

    Add Uppy/TUS

--- Greek Pot Recognition/Program.cs
+++ Greek Pot Recognition/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using tusdotnet;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -12,6 +14,31 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// TUS:
+app.MapTus("/upload", async httpContext => new()
+{
+    // This method is called on each request so different configurations can be returned per user, domain, path etc.
+    // Return null to disable tusdotnet for the current request.
+
+    // Where to store data?
+    MaxAllowedUploadSizeInBytesLong = 10000000,
+    Store = new tusdotnet.Stores.TusDiskStore(Environment.GetEnvironmentVariable("UPLOAD_LOCATION")),
+    Events = new()
+    {
+        // What to do when file is completely uploaded?
+        OnFileCompleteAsync = async eventContext =>
+        {
+            tusdotnet.Interfaces.ITusFile file = await eventContext.GetFileAsync();
+            Dictionary<string, tusdotnet.Models.Metadata> metadata = await file.GetMetadataAsync(eventContext.CancellationToken);
+            using Stream content = await file.GetContentAsync(eventContext.CancellationToken);
+
+            // TODO: Implement
+            //await DoSomeProcessing(content, metadata);
+            Console.WriteLine(metadata);
+        }
+    }
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
